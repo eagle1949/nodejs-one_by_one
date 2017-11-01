0d2bdfdb9e7ebb296e8ed710eaba63b134905f54
Caijw <573301753@qq.com>
2017-11-01 21:02:28 +0800

    tijiao

--- db.js
+++ db.js
@@ -2,14 +2,14 @@
 * @Author: caijw
 * @Date:   2017-10-28 18:32:19
 * @Last Modified by:   caijw
-* @Last Modified time: 2017-10-30 19:32:45
+* @Last Modified time: 2017-11-01 20:54:28
 */
 const knex = require("knex")
 
 const db = knex({
   client: "mysql",
   connection: {
-    host: "rm-wz93wz47ejc698muko.mysql.rds.aliyuncs.com",
+    host: "rm-wz93wz47ejc698muk.mysql.rds.aliyuncs.com",
     user: "root",
     database: "one_by_one",
     password : "Mm!123456"
